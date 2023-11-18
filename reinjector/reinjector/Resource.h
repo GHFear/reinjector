e523a259bd499daf23e133ac85e0127abefac86e
GHFear (Adam J) <32522043+GHFear@users.noreply.github.com>
2023-11-18 20:53:51 +0100

    First version.

---
+++ reinjector/reinjector/Resource.h
@@ -0,0 +1,40 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by reinjector.rc
+
+#define IDD_MAIN_DIALOG 101
+#define IDC_EDIT_PROCESSID 1001
+#define IDC_EDIT_DLLPATH 1002
+#define IDC_BUTTON_INJECT 1003
+#define IDC_BUTTON_BROWSE 1004
+#define IDC_EDIT_SELECTED_FILE 1005
+#define IDC_PROCESS_LIST 1006
+#define ID_LISTBOX 1007
+#define IDC_BUTTON_PROCESSID 1008
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_REINJECTOR_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_REINJECTOR			107
+#define IDI_SMALL				108
+#define IDC_REINJECTOR			109
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
