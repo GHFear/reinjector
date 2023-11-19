ab27d2d3f7862ea17e148bb27db1d7ae0f27a609
GHFear (Adam J) <32522043+GHFear@users.noreply.github.com>
2023-11-19 16:33:57 +0100

    Fixed error with resuming process.

--- reinjector/Injectors/LaunchAndInject/launch_and_inject.h
+++ reinjector/Injectors/LaunchAndInject/launch_and_inject.h
@@ -1,6 +1,40 @@
 #pragma once
 #include "../../reinjector.h"
 
+// Function to resume a suspended process
+BOOL ResumeSuspendedProcess(DWORD processId) {
+    // Open the process with the necessary access rights
+    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
+    if (hProcess == NULL) {
+        // Handle error, e.g., unable to open the process
+        return FALSE;
+    }
+
+    // Enumerate all threads in the process
+    THREADENTRY32 te32;
+    te32.dwSize = sizeof(THREADENTRY32);
+    HANDLE hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, processId);
+    if (Thread32First(hThreadSnap, &te32)) {
+        do {
+            // Open each thread with the necessary access rights
+            HANDLE hThread = OpenThread(THREAD_SUSPEND_RESUME, FALSE, te32.th32ThreadID);
+            if (hThread != NULL) {
+                // Resume the thread
+                ResumeThread(hThread);
+                CloseHandle(hThread);
+            }
+        } while (Thread32Next(hThreadSnap, &te32));
+    }
+
+    // Close the thread snapshot handle
+    CloseHandle(hThreadSnap);
+
+    // Close the process handle
+    CloseHandle(hProcess);
+
+    return TRUE;
+}
+
 bool LaunchAndInject(const wchar_t* targetExecutable, const wchar_t* dllPath)
 {
     // Step 1: Create the process
@@ -76,6 +110,13 @@ bool LaunchAndInject(const wchar_t* targetExecutable, const wchar_t* dllPath)
     CloseHandle(hProcess);
     CloseHandle(processInfo.hThread);
     CloseHandle(processInfo.hProcess);
+
+    
+    if (!ResumeSuspendedProcess(processId))
+    {
+        std::cerr << "Failed to resume the process. Error: " << GetLastError() << std::endl;
+        return false;
+    }
 
     return true;
 }
